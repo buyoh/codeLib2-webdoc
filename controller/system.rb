67fe842fd9f80517c6e3d4ff6ffab743a09fea3f
buyoh <japanjetone@live.jp>
2020-07-25 01:04:51 +0900

    fix reload function

--- controller/system.rb
+++ controller/system.rb
@@ -1,13 +1,16 @@
+# frozen_string_literal: true
 
 updated_time = Time.now
 
 post '/reload' do
-  if Time.now - updated_time > 600
-    update_time = true
+  if Time.now - updated_time > 60
+    updated_time = Time.now
     Thread.start do
-      $sqldb.update_db
+      Dir.chdir("#{$codelib_path}/tools") do
+        $sqldb.update_db
+      end
     end
-    redirect "/", 303
+    redirect '/', 303
   else
     status 406
   end
